88f7c6250db69ddc7c026aa78cc605dc7af006cd
tql <lapduynh72@gmail.com>
2026-01-08 05:45:55 +0700

    fix

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8089/api/v1',
-    apiImage: 'http://localhost:8089/api/v1/products/images/',
+    apiUrl: 'http://api.lap123.click/api/v1',
+    apiImage: 'http://api.lap123.click/api/v1/products/images/',
     newsDefaultImage: 'default-lock-news.jpg'
 };
