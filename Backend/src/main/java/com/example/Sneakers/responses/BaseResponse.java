f35b3d0ca1c028468c67ac01b2ecb08ee797cd17
tql <lapduynh72@gmail.com>
2026-01-09 22:01:44 +0700

    fix

--- Backend/src/main/java/com/example/Sneakers/responses/BaseResponse.java
+++ Backend/src/main/java/com/example/Sneakers/responses/BaseResponse.java
@@ -1,5 +1,6 @@
 package com.example.Sneakers.responses;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
@@ -15,8 +16,10 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 public class BaseResponse {
     @JsonProperty("created_at")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
 
     @JsonProperty("updated_at")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updatedAt;
 }
